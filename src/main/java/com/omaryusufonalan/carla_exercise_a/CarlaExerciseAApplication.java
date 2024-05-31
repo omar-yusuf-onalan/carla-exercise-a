56c652135e5f2d3e86bbbac4449b6e3a16f37f0f
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-05-31 18:10:58 +0300

    Create workspace

---
+++ src/main/java/com/omaryusufonalan/carla_exercise_a/CarlaExerciseAApplication.java
@@ -0,0 +1,13 @@
+package com.omaryusufonalan.carla_exercise_a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarlaExerciseAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarlaExerciseAApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/omaryusufonalan/carla_exercise_a/CarlaExerciseAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omaryusufonalan.carla_exercise_a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarlaExerciseAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
