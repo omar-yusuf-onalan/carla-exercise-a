eca2de28ba9bedb1a421a1c306710059c93ab802
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-05-31 22:09:47 +0300

    Check if column exists

--- src/main/java/com/omaryusufonalan/carla_exercise_a/mapper/CompensationRowMapper.java
+++ src/main/java/com/omaryusufonalan/carla_exercise_a/mapper/CompensationRowMapper.java
@@ -11,17 +11,48 @@ public class CompensationRowMapper implements RowMapper<Compensation> {
     @Override
     public Compensation mapRow(ResultSet rs, int rowNum) throws SQLException {
         Compensation compensation = new Compensation();
-        compensation.setId(rs.getLong("id"));
-        compensation.setTimestamp(rs.getString("timestamp"));
-        compensation.setAgeRange(rs.getString("age_range"));
-        compensation.setIndustry(rs.getString("industry"));
-        compensation.setJobTitle(rs.getString("job_title"));
-        compensation.setAnnualSalary(rs.getDouble("annual_salary"));
-        compensation.setCurrency(rs.getString("currency"));
-        compensation.setLocation(rs.getString("location"));
-        compensation.setPostCollegeExperience(rs.getString("post_college_experience"));
-        compensation.setAdditionalContext(rs.getString("additional_context"));
-        compensation.setOtherCurrency(rs.getString("other_currency"));
+        if (columnExists(rs, "id")) {
+            compensation.setId(rs.getLong("id"));
+        }
+        if (columnExists(rs, "timestamp")) {
+            compensation.setTimestamp(rs.getString("timestamp"));
+        }
+        if (columnExists(rs, "age_range")) {
+            compensation.setAgeRange(rs.getString("age_range"));
+        }
+        if (columnExists(rs, "industry")) {
+            compensation.setIndustry(rs.getString("industry"));
+        }
+        if (columnExists(rs, "job_title")) {
+            compensation.setJobTitle(rs.getString("job_title"));
+        }
+        if (columnExists(rs, "annual_salary")) {
+            compensation.setAnnualSalary(rs.getDouble("annual_salary"));
+        }
+        if (columnExists(rs, "currency")) {
+            compensation.setCurrency(rs.getString("currency"));
+        }
+        if (columnExists(rs, "location")) {
+            compensation.setLocation(rs.getString("location"));
+        }
+        if (columnExists(rs, "post_college_experience")) {
+            compensation.setPostCollegeExperience(rs.getString("post_college_experience"));
+        }
+        if (columnExists(rs, "additional_context")) {
+            compensation.setAdditionalContext(rs.getString("additional_context"));
+        }
+        if (columnExists(rs, "other_currency")) {
+            compensation.setOtherCurrency(rs.getString("other_currency"));
+        }
         return compensation;
+    }
+
+    private boolean columnExists(ResultSet rs, String columnName) throws SQLException {
+        try {
+            rs.findColumn(columnName);
+            return true;
+        } catch (SQLException e) {
+            return false;
+        }
     }
 }
