d7564b3663f46494938a49d19c2bce89cec25768
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-05-31 19:14:41 +0300

    Create CompensationRepository.java

---
+++ src/main/java/com/omaryusufonalan/carla_exercise_a/repository/CompensationRepository.java
@@ -0,0 +1,9 @@
+package com.omaryusufonalan.carla_exercise_a.repository;
+
+import com.omaryusufonalan.carla_exercise_a.entity.Compensation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CompensationRepository extends JpaRepository<Compensation, Long> {
+}
