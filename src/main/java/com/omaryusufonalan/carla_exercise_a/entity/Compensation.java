72fb918896ee0e70ce8a25e26d7753bc4160739d
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-05-31 22:10:27 +0300

    Optimize imports

--- src/main/java/com/omaryusufonalan/carla_exercise_a/entity/Compensation.java
+++ src/main/java/com/omaryusufonalan/carla_exercise_a/entity/Compensation.java
@@ -1,6 +1,9 @@
 package com.omaryusufonalan.carla_exercise_a.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
--- src/main/java/com/omaryusufonalan/carla_exercise_a/repository/CompensationRepository.java
+++ src/main/java/com/omaryusufonalan/carla_exercise_a/repository/CompensationRepository.java
@@ -2,10 +2,7 @@ package com.omaryusufonalan.carla_exercise_a.repository;
 
 import com.omaryusufonalan.carla_exercise_a.entity.Compensation;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface CompensationRepository extends JpaRepository<Compensation, Long> {
